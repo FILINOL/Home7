6d429c0879d52853ad40ab205f21331b2b7db220
FILIN <hagi_beter@mail.ru>
2023-05-28 23:22:22 +0300

    home2

---
+++ Home52/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] arr = new int[3, 4];
+// int m = 3;
+// int n = 4;
+
+void RandomArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+
+void IsNumberInArray()
+{
+    for (int i = 0; i < arr.GetLength(1); i++)
+    { 
+        double sum = 0;
+
+        for (int j = 0; j < arr.GetLength(0); j++)
+        { 
+            sum += arr[j, i];
+        }
+        double average = sum / arr.GetLength(0);
+        Console.WriteLine("Среднее арифметическое элементов столбца {0} равно {1:F1}", i + 1, average);
+    }
+
+}
+
+
+void PrintArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+RandomArray();
+PrintArray();
+IsNumberInArray();
+
