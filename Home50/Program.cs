281aa8afd7efd435a04efba66e6d92b06d0a7123
FILIN <hagi_beter@mail.ru>
2023-05-28 23:28:15 +0300

    repos

--- Home50/Program.cs
+++ Home50/Program.cs
@@ -12,61 +12,6 @@
 // 17 -> такого числа в массиве нет
 
 
-// int[,] arr = new int[3, 4];
-// // int m = 3;
-// // int n = 4;
-// void RandomArray()
-// {
-//     for (int i = 0; i < arr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < arr.GetLength(1); j++)
-//         {
-//             arr[i, j] = new Random().Next(0, 10);
-//         }
-//     }
-// }
-
-// bool IsNumberInArray(int number)
-// {
-//     for (int i = 0; i < arr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < arr.GetLength(1); j++)
-//         {
-//             if (arr[i, j] == number)
-//             {
-//                 Console.WriteLine($"Индексы числа {number} в массиве:[{i},{j}]");
-//                 return true;
-//             }
-//         }
-//     }
-//     Console.WriteLine($"Число {number} не найдено в массиве");
-//     return false;
-// }
-// void PrintArray()
-// {
-//     for (int i = 0; i < arr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < arr.GetLength(1); j++)
-//         {
-//             Console.Write(arr[i, j] + " ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
-
-
-
-// RandomArray();
-// PrintArray();
-// Console.Write("Введите число: ");
-// string text = Console.ReadLine();
-// int number = Convert.ToInt32(text);
-// IsNumberInArray(number);
-// // string text = Console.ReadLine();
-// // Console.WriteLine("Введите число: ");
-// // string text = Console.ReadLine();
-
-
 int[,] arr = new int[3, 4];
 // int m = 3;
 // int n = 4;
@@ -89,16 +34,8 @@ bool IsNumberInArray(int number)
         {
             while (arr[i, j] == number)
             {
-            //    if (arr[i, j] == number)
-            //     {
                     Console.WriteLine($"Индексы числа {number} в массиве:[{i},{j}]");
-                    return  true;  
-                // if()
-                // {
-                //   return  true;  
-                // }
-
-                // return  true;
+                    return  true; 
             }
         }
     }
